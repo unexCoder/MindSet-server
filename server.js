bd82e78f5aa46ebddcd0f7134d48eeb9855eb4c8
Luigi Tamagnini <bosca.music@gmail.com>
2022-02-05 19:36:24 -0300

    dotenv_implementation

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path'); 
 const cors = require('cors');
@@ -8,7 +9,22 @@ const router = require('./routes');
 /////////////////////////////
 // declare server object
 const server = express();
-const PORT = process.env.PORT || 8008;
+const PORT = process.env.PORT;
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PASSWORD;
+
+///////////////////////////////// 
+// conect mongodb with mongoose
+const mongoose = require('mongoose');
+const URI='mongodb+srv://'+db_user+':'+db_password+'@mindsetcluster.fyrnq.mongodb.net/mindset_data?retryWrites=true&w=majority';
+mongoose.connect(URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+.then((result) => console.log('connected to database'))
+.catch((err) => console.log(err));
+
+server.listen(PORT, () => {
+    console.log(">>>>> server running >>>>>");
+    console.log(`>>>>> listening at http://localhost:${PORT} >>>>>`);
+})
 
 // middlewares
 server.use(cors());
@@ -17,21 +33,7 @@ server.use(time_stamper);
 server.use(express.json());
 server.use(express.urlencoded({extended: true}));
 
-// default endpoint
 // server static folder
 server.use(express.static(path.join(__dirname,'public')));
 
-///////////////////////////////// 
-// conect mongodb with mongoose
-const mongoose = require('mongoose');
-const URI='mongodb+srv://mindset_admin:mindset_001@mindsetcluster.fyrnq.mongodb.net/mindset_data?retryWrites=true&w=majority';
-mongoose.connect(URI,{ useNewUrlParser: true, useUnifiedTopology: true })
-.then((result) => console.log('connected to database'))
-.catch((err) => console.log(err));
-
-server.use('/api',router)
-
-server.listen(PORT, () => {
-    console.log(">>>>> server running >>>>>");
-    console.log(`>>>>> listening at http://localhost:${PORT} >>>>>`);
-})
+server.use('/api',router);
